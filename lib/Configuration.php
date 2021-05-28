bdfb8a88e49f7524c7c795cc4309130d80cfed10
pabzoid <pauldickinson@outlook.com>
2021-05-28 21:03:43 +0100

    Update Configuration.php

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -80,7 +80,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = '///shipping/v3';
+    protected $host = 'https://api.royalmail.net/shipping/v3';
 
     /**
      * User agent of the HTTP request, set to "PHP-Swagger" by default
